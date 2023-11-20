0a9c4b685e782e43e807fee99fd9d38d6c619313
BingFengHung <joe94008@gmail.com>
2023-11-20 13:40:26 +0800

    調整按鈕樣式以及背景顏色

--- CodeSnippets/CodeSnippetsToolWindow.cs
+++ CodeSnippets/CodeSnippetsToolWindow.cs
@@ -23,7 +23,7 @@ namespace CodeSnippets
         /// </summary>
         public CodeSnippetsToolWindow() : base(null)
         {
-            this.Caption = "CodeSnippetsToolWindow";
+            this.Caption = "CodeSnippets";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
